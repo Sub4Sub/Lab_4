b9093f32d025a0620cc6943e6e3e0cf1dedbf663
Brett Tindall <sub4subm8.exe@gmail.com>
2021-04-16 19:40:09 -0400

    Added mongoose and connected to localhost db

---
+++ .history/Config/db_20210416193038.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Secret = exports.Path = void 0;
+exports.Path = "mongodb://localhost/webd6201";
---
+++ .history/app_20210416193543.js
@@ -0,0 +1,44 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+let indexRouter = require('./Routes/index');
+
+let app = express();
+
+let DBConfig = require('./Config/db');
+mongoose.connect(DBConfig.Path, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// view engine setup
+app.set('views', path.join(__dirname, 'Views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'Client')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error', { message: err.message, error: res.locals.error, title: 'ERROR', page: 'error'});
+});
+
+module.exports = app;
